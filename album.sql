54643f448cb9bd03921b3328bbfc3780891181eb
abhinav5042 <117156257+abhinav5042@users.noreply.github.com>
2022-11-28 21:04:58 +0530

    Create album.sql

---
+++ album.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "Album"
+(
+    "AlbumId" INT NOT NULL,
+    "Title" VARCHAR(160) NOT NULL,
+    "ArtistId" INT NOT NULL,
+    CONSTRAINT "PK_Album" PRIMARY KEY  ("AlbumId")
+);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (1, 'For Those About To Rock We Salute You', 1);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (2, 'Balls to the Wall', 2);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (3, 'Restless and Wild', 2);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (4, 'Let There Be Rock', 1);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (5, 'Big Ones', 3);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (6, 'Jagged Little Pill', 4);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (7, 'Facelift', 5);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (8, 'Warner 25 Anos', 6);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (9, 'Plays Metallica By Four Cellos', 7);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (10, 'Audioslave', 8);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (11, 'Out Of Exile', 8);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (12, 'BackBeat Soundtrack', 9);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (13, 'The Best Of Billy Cobham', 10);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (14, 'Alcohol Fueled Brewtality Live! [Disc 1]', 11);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (15, 'Alcohol Fueled Brewtality Live! [Disc 2]', 11);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (16, 'Black Sabbath', 12);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (17, 'Black Sabbath Vol. 4 (Remaster)', 12);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (18, 'Body Count', 13);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (19, 'Chemical Wedding', 14);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (20, 'The Best Of Buddy Guy - The Millenium Collection', 15);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (21, 'Prenda Minha', 16);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (22, 'Sozinho Remix Ao Vivo', 16);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (23, 'Minha Historia', 17);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (24, 'Afrociberdelia', 18);
+INSERT INTO "Album" ("AlbumId", "Title", "ArtistId") VALUES (25, 'Da Lama Ao Caos', 18);
+

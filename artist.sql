2ef8efc3f349ae4072fd0f7e75451d23591415ba
abhinav5042 <117156257+abhinav5042@users.noreply.github.com>
2022-11-28 21:05:30 +0530

    Create artist.sql

---
+++ artist.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "Artist"
+(
+    "ArtistId" INT NOT NULL,
+    "Name" VARCHAR(120),
+    CONSTRAINT "PK_Artist" PRIMARY KEY  ("ArtistId")
+);
+
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (1, 'AC/DC');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (2, 'Accept');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (3, 'Aerosmith');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (4, 'Alanis Morissette');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (5, 'Alice In Chains');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (6, 'Antônio Carlos Jobim');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (7, 'Apocalyptica');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (8, 'Audioslave');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (9, 'BackBeat');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (10, 'Billy Cobham');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (11, 'Black Label Society');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (12, 'Black Sabbath');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (13, 'Body Count');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (14, 'Bruce Dickinson');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (15, 'Buddy Guy');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (16, 'Caetano Veloso');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (17, 'Chico Buarque');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (18, 'Chico Science & Nação Zumbi');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (19, 'Cidade Negra');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (20, 'Cláudio Zoli');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (21, 'Various Artists');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (22, 'Led Zeppelin');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (23, 'Frank Zappa & Captain Beefheart');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (24, 'Marcos Valle');
+INSERT INTO "Artist" ("ArtistId", "Name") VALUES (25, 'Milton Nascimento & Bebeto');
